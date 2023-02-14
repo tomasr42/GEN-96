20e6a5e06d888308e363793d3a5a1f852db71e45
tomasr42 <tomas.rosenthal@gmail.com>
2023-02-14 12:21:08 +0200

    fixing tests

--- test/test.ts
+++ test/test.ts
@@ -1,6 +1,6 @@
 function test1(fileName) {
     console.log("Matching on: " + fileName);
-    let match = fileName.match(/^[0-9_]+-MUS.*/);
+    let match = fileName.match(/^[0-9A-Z_]+-MUS.*/);
     if (match) {
         console.log("Match: " + match);
     } else {
@@ -12,8 +12,9 @@ function test1(fileName) {
 let fileName1 = "3_01_3_a3851__-MUS3000R56S.mp4";
 let fileName2 = "3_01_3_3851__-MUS3000R56S.mp4";
 let fileName3 = "3_01_3-3851__-MUS3000R56S.mp4";
+let fileName4 = "2_05_3_M_440_-MUS3000QX4U.mp4";
 
 test1(fileName1);
 test1(fileName2);
 test1(fileName3);
-
+test1(fileName4);

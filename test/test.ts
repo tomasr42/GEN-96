9b976076353ec0aa1a3943b3197cb03e2732c3d6
tomasr42 <tomas.rosenthal@gmail.com>
2023-02-14 11:21:39 +0200

    updating tests

--- test/test.ts
+++ test/test.ts
@@ -1,4 +1,4 @@
-function test(fileName) {
+function test1(fileName) {
     console.log("Matching on: " + fileName);
     let match = fileName.match(/^[0-9_]+-MUS.*/);
     if (match) {
@@ -13,6 +13,7 @@ let fileName1 = "3_01_3_a3851__-MUS3000R56S.mp4";
 let fileName2 = "3_01_3_3851__-MUS3000R56S.mp4";
 let fileName3 = "3_01_3-3851__-MUS3000R56S.mp4";
 
-test(fileName1);
-test(fileName2);
-test(fileName3);
+test1(fileName1);
+test1(fileName2);
+test1(fileName3);
+
